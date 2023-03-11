35fe5d974367f9792053851342d41f12c59495e6
Nauclhlt <111179210+Nauclhlt@users.noreply.github.com>
2023-03-11 13:08:13 +0900

    Initial commit

---
+++ core/NlcEngineGame.cs
@@ -0,0 +1,11 @@
+global using System;
+global using System.Reflection;
+global using System.Runtime.InteropServices;
+global using System.Runtime.CompilerServices;
+global using System.IO;
+global using System.Collections;
+
+public static class NlcEngineGame
+{
+    
+}
